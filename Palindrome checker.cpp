57fa5b5478986fc2a3bb1141369a473ab78a57ad
samlouise <samlouise.morgado.25@usjr.edu.ph>
2026-07-02 12:36:38 +0800

    Refactor palindrome checker code structure

--- Palindrome checker.cpp
+++ Palindrome checker.cpp
@@ -25,36 +25,4 @@ void printArray(int arr[], int size)
     printf("}");
 }
 
-int main()
-{
-    int arr1[] = {1, 2, 3, 2, 1};
-    int arr2[] = {5, 4, 4, 5};
-    int arr3[] = {1, 2, 3, 4, 5};
-    int arr4[] = {7};
 
-    printArray(arr1, 5);
-    if (isPalindrome(arr1, 5))
-        printf(" = PALINDROME\n");
-    else
-        printf(" = NOT PALINDROME\n");
-
-    printArray(arr2, 4);
-    if (isPalindrome(arr2, 4))
-        printf(" = PALINDROME\n");
-    else
-        printf(" = NOT PALINDROME\n");
-
-    printArray(arr3, 5);
-    if (isPalindrome(arr3, 5))
-        printf(" = PALINDROME\n");
-    else
-        printf(" = NOT PALINDROME\n");
-
-    printArray(arr4, 1);
-    if (isPalindrome(arr4, 1))
-        printf(" = PALINDROME\n");
-    else
-        printf(" = NOT PALINDROME\n");
-
-    return 0;
-}
